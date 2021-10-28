3f1f70e2985109b2f3532bfbb10cadf2b93e60cf
jaimemv <jaimemunozvillar@gmail.com>
2021-10-28 23:06:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Webcam from "react-webcam";
 import "./App.css";
 import { nextFrame } from "@tensorflow/tfjs";
 // 2. TODO - Import drawing utility here
-import {drawRect} from "./utilities"; 
+import {drawRect} from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
@@ -13,9 +13,9 @@ function App() {
 
   // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
-    const net = await tf.loadGraphModel('https://livelong.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json')
-    
+    // 3. TODO - Load network
+    const net = await tf.loadGraphModel('https://directionstfodjaime.s3.eu-de.cloud-object-storage.appdomain.cloud/model.json')
+
     // Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -48,17 +48,19 @@ function App() {
       const casted = resized.cast('int32')
       const expanded = casted.expandDims(0)
       const obj = await net.executeAsync(expanded)
-      
-      const boxes = await obj[4].array()
-      const classes = await obj[5].array()
-      const scores = await obj[6].array()
-    
+
+      console.log(await obj[0].array())
+
+      const boxes = await obj[5].array()
+      const classes = await obj[3].array()
+      const scores = await obj[0].array()
+
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
-      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.9, videoWidth, videoHeight, ctx)}); 
+      // drawSomething(obj, ctx)
+      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.9, videoWidth, videoHeight, ctx)});
 
       tf.dispose(img)
       tf.dispose(resized)
@@ -76,7 +78,7 @@ function App() {
       <header className="App-header">
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
           style={{
             position: "absolute",
             marginLeft: "auto",
